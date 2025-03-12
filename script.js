6e86cf41ec7f62ee480c48a518adf23e80658bce
pperjar <pedro1971val@gmail.com>
2025-03-11 22:10:44 -0300

    script

--- script.js
+++ script.js
@@ -16,3 +16,9 @@
     </figure>
 </section>
 */
+const main = document.querySelector('main');//selecionar a tag main
+main.innerHTML="<h2> Cardapio do dia </h2>";
+//alista no js começa no zero.
+const dia Da Semana =['domingo','segunda-feira','terça-feira'.'quarta-feira','quinta-feira','sexta-feira','sabado-letivo']
+const hoje = new Date().getDay();
+//Date informa a data e o getDay()informa qul dos 7(0-6)
