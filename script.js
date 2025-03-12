3456f6ff4020731d34a0e443c89850e83828329c
pperjar <pedro1971val@gmail.com>
2025-03-11 21:43:32 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+<h2>Cardapio do Dia</h2>
+<h3>Segunda-feira</h3>
+<section>
+    <ul>
+    <li>Feijoada</li>
+    <li>Arroz</li>
+    <li>Couve</li>
+    <li>Farofa</li>
+    <li>Torresmo</li>
+    </ul>
+      
+    
+    <figure>
+        <img src="img/image.png" alt="feijoada">
+    </figure>
+</section>
+*/
